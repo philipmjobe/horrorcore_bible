95fb1e35d9cd95a53221f6245579835e8a7d97ba
Philip <philipmjobe@gmail.com>
2022-08-11 14:44:48 -0400

    adds migration to add image to artists

---
+++ db/migrate/20220811184256_add_image_to_artists.rb
@@ -0,0 +1,5 @@
+class AddImageToArtists < ActiveRecord::Migration[6.1]
+  def change
+    add_column :artists, :image, :string
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_08_11_183543) do
+ActiveRecord::Schema.define(version: 2022_08_11_184256) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(version: 2022_08_11_183543) do
     t.string "full_bio"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "image"
   end
 
 end
