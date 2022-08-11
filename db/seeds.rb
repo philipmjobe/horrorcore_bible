91d4efe1d1c5ea1ccdb5ff3ae9820879f3929bf5
Philip <philipmjobe@gmail.com>
2022-08-11 14:51:42 -0400

    adds first artist to seeds file unfortunately it will be the same as my last few apps with no API all artists will be hardcoded

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Artist.create!(name: "Insane Clown Posse", years_active: "1991-present", bio:"Insane Clown Posse (abbreviated as ICP), is an American hip hop duo composed of Violent J (Joseph Bruce) and Shaggy 2 Dope (Joseph Utsler). ICP has earned two platinum and five gold albums. According to Nielsen SoundScan, the entire catalog of the group has sold 6.5 million units in the United States and Canada as of April 2007. The group has established a dedicated following called Juggalos numbering in the 'tens of thousands'.", full_bio: "https://www.last.fm/music/Insane+Clown+Posse/+wiki", image: "https://images.sk-static.com/images/media/profile_images/artists/48999/huge_avatar")
