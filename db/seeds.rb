b51e4548539c9a03cc626adff382eab3b090ed7b
Philip <philipmjobe@gmail.com>
2022-08-17 14:25:41 -0400

    adds Big Stevie Cool

--- db/seeds.rb
+++ db/seeds.rb
@@ -271,7 +271,7 @@ Artist.create!(name: "Big Loony", years_active: "2015-2018", bio: "Big Loony is 
 Artist.create!(name: "Big Que", years_active: "2005-present", bio: "Big Que, born in Galway, Ireland, is a UK based Hip-Hop/Rap artist. He is signed to UK based record label Madness Records. He was one of the artists involved in the UK Juggalo Cypher.
 
 Big Que is due to release a single this month featuring Lex The Hex Master and Peso. He will be releasing his album early next year featuring artists such as Exa, The R.O.C., The Commission, Peso, KidCrusher, and Lex The Hex Master.", image: "https://i0.wp.com/horrorcorewiki.com/wp-content/uploads/2020/10/big-que.jpg?w=450&ssl=1")
-Artist.create!(name:, years_active:, bio:, image:)
+Artist.create!(name: "Big Stevie Cool", years_active: "2001-2006", bio: "Big Stevie Cool was an acid rap emcee from Portland, OR. He was signed to Cryptic Ninja Records alongside the likes of Cadaverz on Kitestrings and Krazy K.", image: "https://i0.wp.com/horrorcorewiki.com/wp-content/uploads/2022/06/big-stevie-cool.jpg?w=450&ssl=1")
 Artist.create!(name:, years_active:, bio:, image:)
 Artist.create!(name:, years_active:, bio:, image:)
 Artist.create!(name:, years_active:, bio:, image:)
