36947e5277e6d6b134b93d12345ee51806fef0e7
Philip <philipmjobe@gmail.com>
2022-09-01 14:06:42 -0400

    adds Project: Deadman

--- db/seeds.rb
+++ db/seeds.rb
@@ -1790,7 +1790,7 @@ In June 2009, went on the 18 city “HELLAVA NIGHT TOUR” with ESHAM. August 20
 PROJECT BORN is very well respected in the game and has done over 200 shows across the country and is in high demand for features. COMING in 2010, PROJECT BORN will release the new album “THE BORN SUPREMACY”, drop a PROJECT BOY ENT. Compilation and head out on a nationwide tour in the summer.
   
 Sadly, it was reported on December 27, 2021 that Pint Capone aka Half Pint passed away.", image: "https://i.discogs.com/2dUXuQEaxsdc4BLZ4iYUZVn1r8Ra1VkqT3POyWcEPec/rs:fit/g:sm/q:40/h:300/w:300/czM6Ly9kaXNjb2dz/LWRhdGFiYXNlLWlt/YWdlcy9SLTE0ODUy/OTMxLTE2MDA1NTA4/NDEtOTcyNC5qcGVn.jpeg")
-Artist.create!(name:, bio:, image:)
+Artist.create!(name: "Project: Deadman", bio: "Detroit area horrorcore rapper Prozak teamed with former Insane Clown Posse producer Mike E. Clark to found the short-lived duo Project: Deadman. One full-length album, Self Inflicted (2004), resulted from the partnership. Prozak had previously been half of the Saginaw-based horrorcore duo Bedlam.", image: "https://i1.sndcdn.com/artworks-RIcL99kkPoH8-0-t500x500.jpg")
 Artist.create!(name:, bio:, image:)
 Artist.create!(name:, bio:, image:)
 Artist.create!(name:, bio:, image:)
