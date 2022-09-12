41cb509c3fddd2312295bfb1ff16f7e63d41a7f6
Philip <philipmjobe@gmail.com>
2022-09-12 15:35:33 -0400

    creates index and show actions in the artists controller

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -1,2 +1,13 @@
 class ArtistsController < ApplicationController
+
+  def index
+    artist = Artist.all
+    render json: artist
+  end
+
+  def show
+    artist = Artist.find_by(id: params[:id])
+    render json: artist
+  end
+  
 end
